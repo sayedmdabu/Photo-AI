a0fd1ca91ca4194f49bc647d126e9138ee166f7c
sayedmdabu <sayedmdabu94@gmail.com>
2023-02-01 21:48:24 +0600

    Logo 01-02-2023

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,8 +1,8 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-            {{-- <img alt="image" src="{{asset('assets/image/logo/Intelcaffe-logo.png')}}" class="header-logo" /> --}}
+            {{-- <x-jet-authentication-card-logo /> --}}
+            <img alt="image" src="{{asset('assets/image/logo/Intelcaffe-logo.png')}}" class="header-logo" />
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
